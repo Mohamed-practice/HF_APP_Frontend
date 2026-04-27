8ca5c6c512406fde00275949138d9f1f042e67c2
Hero Developers <134581766+Herosoftware01@users.noreply.github.com>
2026-04-27 16:16:01 +0530

    Remove duplicate bundle_id from object

--- src/components/cutting_sample/pages/mes_details.jsx
+++ src/components/cutting_sample/pages/mes_details.jsx
@@ -253,7 +253,6 @@ const handleFinalSave = async () => {
         product,
         colour,
         size,
-        bundle_id,
         bf_ironing: bfIroningValue ? 1 : 0,
 af_ironing: afIroningValue ? 1 : 0,
         force_save: forceSave ? 1 : 0,  //  IMPORTANT
